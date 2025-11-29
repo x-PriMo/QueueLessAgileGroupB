9e7634019d2c0a66121bb3f31553cf4d0d5edade
Jakub Kansy <rottef9@gmail.com>
2025-11-29 14:03:33 +0100

    Fix import statement for useParams

--- packages/frontend/src/pages/CompanySettingsPage.tsx
+++ packages/frontend/src/pages/CompanySettingsPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { api, API_URL } from '../lib/api';
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom'; 
 import OwnerScheduleManager from '../components/OwnerScheduleManager';
 
 interface Company {
